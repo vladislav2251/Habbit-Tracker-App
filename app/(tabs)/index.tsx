846c9fc9b5a8a6ce6ac82a9dcd0f97f995c33a5e
Vlad Didyk <vladdidyk1985@gmail.com>
2025-05-05 09:36:14 +0300

    Revert "chore: project re-setup"
    This reverts commit 3b6c71273a3b21876c496c18ef3ceefe1a77974f.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import React from 'react';
 
 export default function HomeScreen() {
   return (
