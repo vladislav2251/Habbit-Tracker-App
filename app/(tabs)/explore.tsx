3b6c71273a3b21876c496c18ef3ceefe1a77974f
Vlad Didyk <vladdidyk1985@gmail.com>
2025-05-05 09:26:33 +0300

    chore: project re-setup

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -7,6 +7,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import React from 'react';
 
 export default function TabTwoScreen() {
   return (
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import React from 'react';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
